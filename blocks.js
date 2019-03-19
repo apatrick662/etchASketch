95ee4eea9cb543be20f0e74790ccb6cce65f78c9
apatrick662 <apatrick662@gmail.com>
2019-03-18 19:56:04 -0500

    Reset button works

--- blocks.js
+++ blocks.js
@@ -1,11 +1,26 @@
 const container = document.getElementById("container");
+const button = document.getElementById("reset");
 
-var divArr = [];
+var grid = [];
 
-for(i = 0; i < 16; i++){
+for (i = 0; i < 16; i++){
     var block = document.createElement("div");
     block.classList.add('block');
-    divArr.push(block);
+    container.appendChild(block);
+    grid.push(block);
 }
 
+function resetGrid() {
+    var gridSize = prompt("How big do you want the grid?(n x n)")
 
+    grid = [];
+
+    for (i = 0; i < Math.pow(gridSize, 2); i++) {
+        var block = document.createElement("div");
+        block.classList.add('block');
+        container.appendChild(block);
+        grid.push(block);
+    }
+}
+
+button.addEventListener("click", resetGrid);
