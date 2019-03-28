6d20f831fa112368bf0c71d12866573a2d67c6b1
apatrick662 <apatrick662@gmail.com>
2019-03-27 20:21:47 -0500

    removed commented out switch statement

--- blocks.js
+++ blocks.js
@@ -18,35 +18,6 @@ for (i = 0; i < Math.pow(16, 2); i++){
 }
 
 function changeColor() {
-    // switch(color.value){
-    //     case "black":
-    //         this.style.backgroundColor = "black";
-    //         break;
-    //     case "gray":
-    //         this.style.backgroundColor = "dimgray";
-    //         break;
-    //     case "red":
-    //         this.style.backgroundColor = "red";
-    //         break;
-    //     case "blue":
-    //         this.style.backgroundColor = "blue";
-    //         break;
-    //     case "green":
-    //         this.style.backgroundColor = "green";
-    //         break;
-    //     case "yellow":
-    //         this.style.backgroundColor = "yellow";
-    //         break;
-    //     case "orange":
-    //         this.style.backgroundColor = "orange";
-    //         break;
-    //     case "purple":
-    //         this.style.backgroundColor = "darkviolet";
-    //         break;
-    //     case "pink":
-    //         this.style.backgroundColor = "deeppink";
-    //         break;
-    // }
 
     var etchColor = color.value;
     this.style.backgroundColor = etchColor;
